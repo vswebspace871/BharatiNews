25f82950f52c31ebed1b848dfe6763aed5ab248e
Vaibhav <vswebspace871@gmail.com>
2022-04-17 14:29:44 +0530

    Splash Screen Done

---
+++ app/src/main/java/com/example/bharatinews/Activities/SplashScreen.java
@@ -0,0 +1,23 @@
+package com.example.bharatinews.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.bharatinews.MainActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
+            }
+        }, 1500);
+    }
+}
