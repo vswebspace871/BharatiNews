5ce1dcd27568228a718b24aaa36f2696cb3d6fe7
Vaibhav <vswebspace871@gmail.com>
2022-04-21 12:41:02 +0530

    custom App Bar created

---
+++ app/src/main/java/com/example/bharatinews/Activities/SettingsActivity.java
@@ -0,0 +1,31 @@
+package com.example.bharatinews.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.bharatinews.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.settings);
+        //   Deprecated = toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_baseline_arrow_back_24));
+        toolbar.setNavigationIcon(ContextCompat.getDrawable(this, R.drawable.ic_baseline_arrow_back_24));
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+}
